af441f5baf3acfbd9a309c2a92d15c5bd547dbd4
Eric Berger <eric@brrgrr.com>
2017-09-19 09:23:39 -0500

    Breakout style, header, form and footer

--- main.py
+++ main.py
@@ -2,43 +2,59 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
-form = """
+
+styles = """
+form {
+    background-color: #eee;
+    padding: 20px;
+    margin: 0 auto;
+    width: 540px;
+    font: 16px sans-serif;
+    border-radius: 10px;
+}
+textarea {
+    margin: 10px 0;
+    width: 540px;
+    height: 120px;
+}
+"""
+
+page_header = """
 <!DOCTYPE html>
 
 <html>
     <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <title>Web Caesar</title>
         <style>
-            form {
-                background-color: #eee;
-                padding: 20px;
-                margin: 0 auto;
-                width: 540px;
-                font: 16px sans-serif;
-                border-radius: 10px;
-            }
-            textarea {
-                margin: 10px 0;
-                width: 540px;
-                height: 120px;
-            }
+            """ + styles + """
         </style>
     </head>
     <body>
-		<form action="/" method="post">
-	        <label for="rot">Rotate by:</label>
-	        <input type="text" name="rot" value="0" />
-	        <textarea name="text"></textarea>
-	        <br>
-	        <input type="submit" value="Submit Query" />
-	    </form>
+"""
+
+page_footer = """
     </body>
 </html>
+"""
+
+form = """
+    <form action="/" method="post">
+        <label for="rot">Rotate by:</label>
+        <input type="text" name="rot" value="0" />
+        <textarea name="text"></textarea>
+        <br>
+        <input type="submit" value="Submit Query" />
+    </form>
 """
 
 
 @app.route("/")
 def index():
-    return form
+    content = page_header + form + page_footer
+    return content
 
 
 app.run()
