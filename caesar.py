67561ad61174f8422193e8907a0892ac0406fb8e
Eric Berger <eric@brrgrr.com>
2017-09-19 10:25:07 -0500

    Updated to replace alphabet strings ('abcdef...') in favor of ascii_letters/lower/upper

--- caesar.py
+++ caesar.py
@@ -1,39 +1,34 @@
+import string
+
+
 def alphabet_position(character):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    lower = character.lower()
-    return alphabet.index(lower)
+    return string.ascii_letters.find(character) % 26
+
 
 def rotate_string_13(text):
-
     rotated = ''
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-
     for char in text:
         rotated_idx = (alphabet_position(char) + 13) % 26
         if char.isupper():
-            rotated = rotated + alphabet[rotated_idx].upper()
+            rotated = rotated + string.ascii_uppercase[rotated_idx]
         else:
-            rotated = rotated + alphabet[rotated_idx]
-
+            rotated = rotated + string.ascii_lowercase[rotated_idx]
     return rotated
 
-def rotate_character(char, rot):
-    alphabet = 'abcdefghijklmnopqrstuvwxyz'
-    rotated_idx = (alphabet_position(char) + rot) % 26
 
+def rotate_character(char, rot):
+    rotated_idx = (alphabet_position(char) + rot) % 26
     if char.isupper():
-        return alphabet[rotated_idx].upper()
+        return string.ascii_uppercase[rotated_idx]
     else:
-        return alphabet[rotated_idx]
+        return string.ascii_lowercase[rotated_idx]
+
 
 def rotate_string(text, rot):
-
     rotated = ''
-
     for char in text:
         if (char.isalpha()):
             rotated = rotated + rotate_character(char, rot)
         else:
             rotated = rotated + char
-
     return rotated
